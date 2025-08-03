9cca9c4b1bb48a01b35a0f55ae5c7497d2e85296
mesias <gmcampo@sudamericano.edu.ec>
2025-08-03 14:56:38 -0500

    fix?

--- project/src/api/validation.ts
+++ project/src/api/validation.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = process.env.REACT_APP_API_URL || "http://localhost:8080/validation";
+const API = process.env.REACT_APP_API_URL || "/api/validation";
 export interface PlayerPending {
   id: number;
   name: string;
